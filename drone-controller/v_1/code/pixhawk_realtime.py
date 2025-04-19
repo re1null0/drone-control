1161a953aa53c6802717ede994a9a61575ab20ec
cedrichld <cedric.hollande25@gmail.com>
2025-04-19 16:48:29 -0400

    Uploaded Custom firmware. Now preparing to control motors individually

--- drone-controller/v_1/code/pixhawk_realtime.py
+++ drone-controller/v_1/code/pixhawk_realtime.py
@@ -32,11 +32,11 @@ class Pixhawk_Control(object):
         time.sleep(1)
 
         # Optionally set GUIDED_NOGPS "GUID_OPTIONS" so it will accept attitude control
-        print("Enabling external attitude control (GUID_OPTIONS = 8)...")
+        print("Enabling external attitude control (GUID_OPTIONS = 8 - float 7)...")
         master.mav.param_set_send(
             master.target_system, master.target_component,
             b'GUID_OPTIONS',
-            float(8), # SHOULDNT THIS BE 8? IT WAS AT 7
+            float(7),
             mavutil.mavlink.MAV_PARAM_TYPE_INT32
         )
         time.sleep(1)
